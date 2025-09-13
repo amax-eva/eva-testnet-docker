d49c3fd00f164cc16d0bda75f6f042921c60fc69
pangolin <hub500@protonmail.com>
2025-09-13 22:02:59 +0800

    run sh

--- erigon-init.sh
+++ erigon-init.sh
@@ -1,3 +1,9 @@
 rm -rf ./data/erigon
+rm -rf ./data/beacon
+rm -rf ./data/validator
+
 mkdir -p ./data/erigon
+mkdir -p ./data/beacon
+mkdir -p ./data/validator
+
 erigon init --datadir=./data/erigon ./network/genesis.json
